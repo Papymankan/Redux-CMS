1eeb98bbf8216804a7c16fd66cdf23cb0a4a4c7b
Papymankan <parsa84rostami@gmail.com>
2024-08-19 19:32:26 +0300

    first commit

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
