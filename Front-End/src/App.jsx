c4e28d89134db5ec3e0461bacf7455e7bcd262a5
Papymankan <parsa84rostami@gmail.com>
2024-08-20 09:54:18 +0330

    sidebar ui

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import './App.css'
+import SideBar from './Components/SideBar/SideBar'
 
 export default function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <div className="container px-0">
+        <main className='main'>
+          <div className="row justify-content-between mx-0">
+            <SideBar />
+          </div>
+        </main>
+      </div>
     </>
   )
 }
