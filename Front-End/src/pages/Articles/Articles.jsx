cbdfde6a960c4410946207950c6cb58977c0d9f1
Papymankan <parsa84rostami@gmail.com>
2024-08-25 18:59:48 +0330

    add article ui

--- Front-End/src/pages/Articles/Articles.jsx
+++ Front-End/src/pages/Articles/Articles.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ArticleBox from "./../../components/ArticleBox/ArticleBox";
 
@@ -6,15 +6,26 @@ import "./Articles.css";
 import store from "../../Redux/store";
 import { useSelector } from "react-redux";
 import { fetchArticles } from "../../Redux/Reducer/articles";
+import { fetchCategories } from "../../Redux/Reducer/categories";
 
 export default function Articles() {
 
 
   useEffect(() => {
     store.dispatch(fetchArticles('https://redux-cms.iran.liara.run/api/articles'))
+    store.dispatch(fetchCategories('https://redux-cms.iran.liara.run/api/categories'))
   }, [])
 
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [views, setViews] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const [isShowModal, setIsShowModal] = useState(false)
+
   const articles = useSelector(state => state.articles)
+  const categories = useSelector(state => state.categories)
+
 
   return (
     <div className="col-8 content px-0">
@@ -46,7 +57,113 @@ export default function Articles() {
             </Link>
           </li>
         </ul>
-        <div className="active"></div>
+
+        <div
+          className={`modal ${isShowModal ? "show-modal" : null}`}
+          id="show-info-modal"
+        >
+          <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">دوره جدید</h4>
+                <button
+                  type="button"
+                  class="btn-close py-0"
+                  data-bs-dismiss="modal"
+                  onClick={() => setIsShowModal(false)}
+                ></button>
+              </div>
+
+              <div class="modal-body position-relative">
+                <form action="#" class="form row mx-0">
+                  <div class="form__box-input col-12 px-2">
+                    <span class="fa fa-comment form__icon icon-name-article"></span>
+                    <input
+                      placeholder={'عنوان'}
+                      type="text"
+                      name=""
+                      id="firstname"
+                      onChange={(e) => {
+                        setTitle(e.target.value)
+                      }}
+                      value={title}
+                      class="form-control form__input input-user-firstname"
+                    />
+                  </div>
+
+                  <div class="form__box-input col-12 px-2">
+                    <span class="fa fa-bars form__icon"></span>
+                    <select name="category"
+                      id="categories-box"
+                      onChange={(e) => {
+                        setCategory(e.target.value)
+                      }}
+                      value={category}
+                    >
+                      <option value="none" hidden selected>دسته بندی</option>
+                      {
+                        categories.map(cat => <option value={cat.title}>{cat.title}</option>)
+                      }
+
+                    </select>
+                  </div>
+
+                  <div class="form__box-input col-12 px-2">
+                    <span class="fa fa-users form__icon"></span>
+                    <input
+                      lang="en"
+                      placeholder={'تعداد بازدید ها'}
+                      type="number"
+                      name=""
+                      onChange={(e) => {
+                        setViews(e.target.value)
+                      }}
+                      value={views}
+                      id="email"
+                      class="form-control form__input input-user-email"
+                    />
+                  </div>
+
+                  <div class="form__box-input col-12 px-2">
+                    <span class="fa fa-paragraph form__icon"></span>
+                    <input
+                      placeholder={'توضیحات'}
+                      type="text"
+                      name=""
+                      onChange={(e) => {
+                        setDesc(e.target.value)
+                      }}
+                      value={desc}
+                      id="count-product"
+                      class="form-control form__input input-user-product"
+                      readonly
+                    />
+
+                  </div>
+                </form>
+              </div>
+
+              <div class="modal-footer">
+                <button
+                  class="btn btn-primary btn-lg"
+                  data-bs-dismiss="modal"
+                // onClick={() => addNewCourse()}
+                >
+                  افزودن
+                </button>
+
+                <button
+                  class="btn btn-danger btn-lg"
+                  data-bs-dismiss="modal"
+                  onClick={() => setIsShowModal(false)}
+                >
+                  بستن
+                </button>
+
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="articles">
           <div className="articles__list">
@@ -65,6 +182,7 @@ export default function Articles() {
             data-bs-toggle="modal"
             data-bs-target="#new-article"
             id="btn-modal-new-article"
+            onClick={()=>setIsShowModal(true)}
           >
             افزودن مقاله جدید
           </button>
